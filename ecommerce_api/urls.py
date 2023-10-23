89592ab858ae71e04c6de9cf05711000906aeec6
Anurag Chauhan <55919146+ianuragg@users.noreply.github.com>
2023-10-23 12:02:23 +0530

    added static file urls

--- ecommerce_api/urls.py
+++ ecommerce_api/urls.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from django.conf import settings
+from django.views.static import serve
 
 urlpatterns = [
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
     path('admin/', admin.site.urls),
     #API URLs
     path('', include("api.urls")),
